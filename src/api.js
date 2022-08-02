f2f8954f8a74ce204784f931e8e0ea905fe35e22
lee.huimok <hellowin6076@gmail.com>
2022-08-02 16:32:30 +0900

    Fetching and mapping data from weather API

--- src/api.js
+++ src/api.js
@@ -1,10 +1,13 @@
-export const GEOAPIKEY = process.env.REACT_APP_GEOAPIKEY
+export const GEO_API_KEY = process.env.REACT_APP_GEO_API_KEY
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": GEOAPIKEY,
+    "X-RapidAPI-Key": GEO_API_KEY,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 }
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,36 +1,42 @@
 import "./current-weather.css"
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
   return (
     <div className="weather">
       <div className="top">
         <div>
-          <p className="city">Osaka</p>
-          <p className="weather-description">Sunny</p>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
         </div>
-        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+        <img
+          alt="weather"
+          className="weather-icon"
+          src={`icons/${data.weather[0].icon}.png`}
+        />
       </div>
       <div className="bottom">
-        <p className="temperature">18°C</p>
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
         <div className="details">
           <div className="parameter-row">
-            <span className="parameter-label">Details</span>
+            <span className="parameter-label">상세</span>
           </div>
           <div className="parameter-row">
-            <span className="parameter-label">Feels like</span>
-            <span className="parameter-value">22°C</span>
+            <span className="parameter-label">체감온도</span>
+            <span className="parameter-value">
+              {Math.round(data.main.feels_like)}°C
+            </span>
           </div>
           <div className="parameter-row">
-            <span className="parameter-label">Wind</span>
-            <span className="parameter-value">2 m/s</span>
+            <span className="parameter-label">바람</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
           </div>
           <div className="parameter-row">
-            <span className="parameter-label">Humidity</span>
-            <span className="parameter-value">15%</span>
+            <span className="parameter-label">습도</span>
+            <span className="parameter-value">{data.main.humidity} %</span>
           </div>
           <div className="parameter-row">
-            <span className="parameter-label">Pressure</span>
-            <span className="parameter-value">15 hPa</span>
+            <span className="parameter-label">기압</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>
